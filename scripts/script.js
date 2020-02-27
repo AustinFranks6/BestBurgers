2e08a8dd240450347e8101850058c7aff297b19c
Franks40e <austinfranks704@gmail.com>
2020-02-27 00:47:37 -0500

    lastupdate

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,21 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+//THUMBNAIL GALLERY
+
+function imageGallery() {
+  const highlight = document.querySelector(".menu-display");
+  const previews = document.querySelectorAll(".menu img");
+
+  previews.forEach(preview => {
+    preview.addEventListener("click", function() {
+      const smallSrc = this.src;
+      const bigSrc = smallSrc.replace("small", "big");
+      highlight.src = bigSrc;
+      preview.classList.add("foodactive");
+    });
+  });
+}
+
+imageGallery();
